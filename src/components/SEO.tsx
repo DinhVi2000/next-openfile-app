d05f31400145b0c4f3fdc1b2a34aa6be32d65967
vind <vind@stisolutions.net>
2023-04-11 15:32:14 +0700

    add seo data

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -10,16 +10,30 @@ interface Props {
 const SEO = ({ title, description, image }: Props) => {
   return (
     <Head>
+      <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, user-scalable=no"
+      />
+
+      {/* info */}
       <title>{title}</title>
+      <meta name="title" content={title}></meta>
       <meta name="description" content={description} />
 
       {/* facebook */}
       <meta property="og:title" content={title}></meta>
-      <meta property="og:description" content={description}></meta>
-      <meta property="og:locale" content="en"></meta>
+      <meta property="og:type" content="website"></meta>
+      <meta property="og:image" content={image}></meta>
+      {/* <meta property="og:url" content={domain}></meta> */}
+      <meta property="og:description" content={description} />
 
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      {/* twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      {/* <meta property="twitter:url" content={domain} /> */}
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image}></meta>
     </Head>
   );
 };
