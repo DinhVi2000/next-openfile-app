22f32ca98430459aac68356cc36d388cff748472
vind <vind@stisolutions.net>
2023-04-11 11:49:20 +0700

    remove data-react-helmet

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -14,13 +14,10 @@ const SEO = ({ title, description, image }: Props) => {
       <meta name="description" content={description} />
 
       {/* facebook */}
-      <meta data-react-helmet="true" property="og:title" content={title}></meta>
-      <meta
-        data-react-helmet="true"
-        property="og:description"
-        content={description}
-      ></meta>
-      <meta data-react-helmet="true" property="og:locale" content="en"></meta>
+      <meta property="og:title" content={title}></meta>
+      <meta property="og:description" content={description}></meta>
+      <meta property="og:locale" content="en"></meta>
+
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
